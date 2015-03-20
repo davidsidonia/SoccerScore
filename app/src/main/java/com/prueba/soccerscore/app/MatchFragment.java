c8186dfe08ad5a7313d64483581f7b70a5188a8e
David <davidsidonia@hotmail.com>
2015-03-20 23:39:59 +0100

    Initial commit

---
+++ app/src/main/java/com/prueba/soccerscore/app/MatchFragment.java
@@ -0,0 +1,177 @@
+package com.prueba.soccerscore.app;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.*;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MatchFragment extends Fragment {
+
+    private ArrayAdapter<String> matchs;
+
+    public MatchFragment() {
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Add this line in order for this fragment to handle menu events.
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.matchfragment, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_refresh) {
+            FetchMatch fetchMatch = new FetchMatch();
+            fetchMatch.execute();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        // Create some dummy data for the ListView. Here's a sample weekly forecast
+        String[] data = {
+                "Barcelona",
+                "Sevilla",
+                "Betis",
+                "Recre",
+                "Santander",
+                "Jar kgv",
+                "Barcelona2",
+                "Sevilla2",
+                "Betis2",
+                "Recre2",
+                "Santander2",
+                "Jar kgv2",
+                "Barcelona3",
+                "Sevilla3",
+                "Betis3",
+                "Recre3",
+                "Santander3",
+                "Jar kgv3",
+                "fgwrbrb3"
+        };
+        List<String> local = new ArrayList<String>(Arrays.asList(data));
+
+// Now that we have some dummy forecast data, create an ArrayAdapter.
+        // The ArrayAdapter will take data from a source (like our dummy forecast) and
+        // use it to populate the ListView it's attached to.
+        matchs =
+                new ArrayAdapter<String>(
+                        getActivity(), // The current context (this activity)
+                        R.layout.list_item_match, // The name of the layout ID.
+                        R.id.list_item_local_team, // The ID of the textview to populate.
+                        local);
+
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        ListView listView = (ListView) rootView.findViewById(R.id.listview_match);
+        listView.setAdapter(matchs);
+
+
+        return rootView;
+    }
+
+
+    public class FetchMatch extends AsyncTask<Void, Void, Void> {
+
+        private final String LOG_TAG = FetchMatch.class.getSimpleName();
+
+        @Override
+        protected Void doInBackground(Void... params) {
+
+
+            // These two need to be declared outside the try/catch
+            // so that they can be closed in the finally block.
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            // Will contain the raw JSON response as a string.
+            String matchsJsonStr = null;
+
+            try {
+                // Construct the URL for the resultados-futbol query
+
+                URL url = new URL("http://www.resultados-futbol.com/scripts/api/api.php?format=json&req=matchs&key=7e210fae6e101bc9a25b2b432d00e501&league=1");
+
+                // Create the request to OpenWeatherMap, and open the connection
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                    // But it does make debugging a *lot* easier if you print out the completed
+                    // buffer for debugging.
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    // Stream was empty. No point in parsing.
+                    return null;
+                }
+                matchsJsonStr = buffer.toString();
+            } catch (IOException e) {
+                Log.e("LOG_TAG", "Error ", e);
+                // If the code didn't successfully get the weather data, there's no point in attemping
+                // to parse it.
+                return null;
+
+
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e("LOG_TAG", "Error closing stream", e);
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}
