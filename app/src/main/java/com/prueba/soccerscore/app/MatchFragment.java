b3a0fb2e6033edf8b0bd597d504eff61949acdc2
David <davidsidonia@hotmail.com>
2015-03-21 01:28:08 +0100

    Unidad 2.09 (20-03-15)

--- app/src/main/java/com/prueba/soccerscore/app/MatchFragment.java
+++ app/src/main/java/com/prueba/soccerscore/app/MatchFragment.java
@@ -7,6 +7,9 @@ import android.util.Log;
 import android.view.*;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -97,12 +100,100 @@ public class MatchFragment extends Fragment {
     }
 
 
-    public class FetchMatch extends AsyncTask<Void, Void, Void> {
+    public class FetchMatch extends AsyncTask<Void, Void, String[]> {
 
         private final String LOG_TAG = FetchMatch.class.getSimpleName();
 
+
+        /**
+         * + * Take the String representing the complete forecast in JSON Format and
+         * + * pull out the data we need to construct the Strings needed for the wireframes.
+         * + *
+         * + * Fortunately parsing is easy: constructor takes the JSON string and converts it
+         * + * into an Object hierarchy for us.
+         * +
+         */
+        private String[] getMatchDataFromJson(String matchJsonStr)
+                throws JSONException {
+
+            // These are the names of the JSON objects that need to be extracted.
+            final String OWM_MATCH = "match";
+            final String OWM_ID = "id";
+            final String OWM_ROUND = "round";
+            final String OWM_LOCAL = "local";
+            final String OWM_VISITOR = "visitor";
+            final String OWM_SCHEDULE = "schedule";
+            final String OWM_DATE = "date";
+            final String OWM_HOUR = "hour";
+            final String OWM_MINUTE = "minute";
+            final String OWM_RESULT = "result";
+            final String OWM_LIVE_MINUTE = "live_minute";
+
+            JSONObject matchJson = new JSONObject(matchJsonStr);
+            JSONArray matchArray = matchJson.getJSONArray(OWM_MATCH);
+
+            // OWM returns daily forecasts based upon the local time of the city that is being
+            // asked for, which means that we need to know the GMT offset to translate this data
+            // properly.
+
+            // Since this data is also sent in-order and the first day is always the
+            // current day, we're going to take advantage of that to get a nice
+            // normalized UTC date for all of our weather.
+
+
+//**************************** en el tamaño del array 'resultStrs'  no se si va a funcionar
+            String[] resultStrs = new String[matchArray.length()];
+
+            for (int i = 0; i < matchArray.length(); i++) {
+                // For now, using the format "Day, description, hi/low"
+                String local;
+                String result;
+                String visitor;
+
+                // Get the JSON object representing the day
+                JSONObject match = matchArray.getJSONObject(i);
+
+                local = match.getString(OWM_LOCAL);
+                result = match.getString(OWM_RESULT);
+                visitor = match.getString(OWM_VISITOR);
+
+
+//**************  comentado por mi, porque es de sunshine y me puede servir de guia para SoccerScore
+//// The date/time is returned as a long. We need to convert that
+//// into something human-readable, since most people won't read "1400356800" as
+//// "this saturday".
+//                long dateTime;
+//// Cheating to convert this to UTC time, which is what we want anyhow
+//                dateTime = dayTime.setJulianDay(julianStartDay+i);
+//                day = getReadableDateString(dateTime);
+//
+//
+//                // description is in a child array called "weather", which is 1 element long.
+//                JSONObject weatherObject = dayForecast.getJSONArray(OWM_WEATHER).getJSONObject(0);
+//                description = weatherObject.getString(OWM_DESCRIPTION);
+//
+//                // Temperatures are in a child object called "temp". Try not to name variables
+//                // "temp" when working with temperature. It confuses everybody.
+//                JSONObject temperatureObject = dayForecast.getJSONObject(OWM_TEMPERATURE);
+//                double high = temperatureObject.getDouble(OWM_MAX);
+//                double low = temperatureObject.getDouble(OWM_MIN);
+//
+//                highAndLow = formatHighLows(high, low);
+//***************************************************************************************
+
+                resultStrs[i] = local + " / " + result + " / " + visitor;
+            }
+
+            for (String s : resultStrs) {
+                Log.v(LOG_TAG, "Match entry: " + s);
+            }
+            return resultStrs;
+
+        }
+
+
         @Override
-        protected Void doInBackground(Void... params) {
+        protected String[] doInBackground(Void... params) {
 
 
             // These two need to be declared outside the try/catch
@@ -145,6 +236,9 @@ public class MatchFragment extends Fragment {
                     return null;
                 }
                 matchsJsonStr = buffer.toString();
+
+                Log.v(LOG_TAG, "Match string: " + matchsJsonStr);
+
             } catch (IOException e) {
                 Log.e("LOG_TAG", "Error ", e);
                 // If the code didn't successfully get the weather data, there's no point in attemping
@@ -164,7 +258,31 @@ public class MatchFragment extends Fragment {
                     }
                 }
             }
+
+            try {
+                return getMatchDataFromJson(matchsJsonStr);
+            } catch (JSONException e) {
+                Log.e(LOG_TAG, e.getMessage(), e);
+                e.printStackTrace();
+            }
+
+            // This will only happen if there was an error getting or parsing the forecast.
+
+
             return null;
         }
+
+
+        @Override
+        protected void onPostExecute(String[] result) {
+            if (result != null) {
+                matchs.clear();
+                for (String matchStr : result) {
+                    matchs.add(matchStr);
+                }
+                // New data is back from the server. Hooray!
+            }
+        }
+
     }
 }
