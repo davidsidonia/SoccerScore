b7efd6df8345b9badeb54aedc9f84b05663c9eee
David <davidsidonia@hotmail.com>
2015-04-03 12:58:08 +0200

    29. (03-04-15) Quitado menu Setting y menu Refresh puesto visible

--- app/src/main/java/com/prueba/soccerscore/app/MainActivity.java
+++ app/src/main/java/com/prueba/soccerscore/app/MainActivity.java
@@ -4,8 +4,6 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 
 /*
  * Created by David on 26/03/2015.
@@ -13,6 +11,7 @@ import android.view.MenuItem;
 public class MainActivity extends ActionBarActivity implements MatchFragment.Callback {
 
     private boolean mTwoPane;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,19 +27,8 @@ public class MainActivity extends ActionBarActivity implements MatchFragment.Cal
             mTwoPane = false;
         }
     }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+
+
     @Override
     public void onItemSelected(Uri contentUri) {
         if (mTwoPane) {
--- app/src/main/java/com/prueba/soccerscore/app/ScoreActivity.java
+++ app/src/main/java/com/prueba/soccerscore/app/ScoreActivity.java
@@ -2,8 +2,6 @@ package com.prueba.soccerscore.app;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 
 /*
  * Created by David on 26/03/2015.
@@ -24,17 +22,5 @@ public class ScoreActivity extends ActionBarActivity {
                     .commit();
         }
     }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.score, menu);
-        return true;
-    }
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+
 }
